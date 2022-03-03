169c7f5e25ba61da0ab8acff6a271d9e8c4b2c23
pbalty <pierre.balty@uclouvain.be>
2022-03-03 17:39:25 +0100

    :construction: Adding the tar-list file

---
+++ tar-list.sh
@@ -0,0 +1,20 @@
+# Ucx
+wget 'https://github.com/openucx/ucx/releases/download/v1.12.0/ucx-1.12.0.tar.gz'
+
+# Ofi
+wget 'https://github.com/ofiwg/libfabric/archive/refs/tags/v1.14.0.tar.gz'
+
+# Open-mpi
+wget 'https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.2.tar.gz'
+
+# Open Blas
+wget 'https://github.com/xianyi/OpenBLAS/archive/v0.3.18.tar.gz'
+
+# HDF5
+wget 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.bz2'
+
+# p4est
+wget 'https://p4est.github.io/release/p4est-2.8.tar.gz'
+
+# FFTW
+wget 'http://www.fftw.org/fftw-3.3.10.tar.gz'
