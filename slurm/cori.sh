47b472ee8857c32c82f6173ec612069a08acf571
Thomas Gillis <thoma.gillis@gmail.com>
2022-05-09 08:41:27 -0700

    fixes modules in slurm file

--- slurm/cori.sh
+++ slurm/cori.sh
@@ -6,7 +6,7 @@
 #SBATCH --constraint=haswell
 
 #-------------------------------------------------------------------------------
-module swap PrgEnv- PrgEnv-gnu/6.0.10
+module swap PrgEnv-${PE_ENV,,} PrgEnv-gnu/6.0.10
 module load cray-hdf5-parallel/1.10.5.2
 module unload darshan
 module use /global/common/software/m3169/cori/modulefiles
